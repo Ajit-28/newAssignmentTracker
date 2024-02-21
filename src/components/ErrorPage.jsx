6ed12b1d1f08c1f90e33f9a936ad97f84b738399
Ajit Pal <palajit28@gmail.com>
2024-02-21 17:30:35 +0530

    authentication code added

--- src/components/ErrorPage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function ErrorPage() {
-  return (
-    <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '100vh'}}>
-      <h1> 404 Error Page Not Found</h1>
-    </div>
-  )
-}
